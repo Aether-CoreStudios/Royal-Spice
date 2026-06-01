6de2f0735d69bcfe865808f338c4b1fe3b437234
Aether-CoreStudios <sergioberlin325@gmail.com>
2026-06-01 19:29:25 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const invoiceRoutes = require("./routes/invoiceRoutes");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://royal-spice-alpha.vercel.app"],
     credentials: true,
   }),
 );
