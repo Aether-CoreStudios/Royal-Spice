ff8f64cbb56fb995171dfcd5bd3c1b509197fd95
Aether-CoreStudios <sergioberlin325@gmail.com>
2026-06-05 21:53:26 +0530

    Added GET all orders route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -74,7 +74,23 @@ Thank you for ordering from Royal Spice.`,
     });
   }
 });
+/* =========================
+   GET ALL ORDERS
+========================= */
 
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find().sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 /* =========================
    GET SINGLE ORDER
 ========================= */
